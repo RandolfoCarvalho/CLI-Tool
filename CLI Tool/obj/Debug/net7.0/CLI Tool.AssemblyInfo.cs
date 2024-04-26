98f6c47b9950e5078f77c6f841d5ba054d899d7c
RandolfoCarvalho <randolfoirades@gmail.com>
2024-04-25 23:43:30 -0300

    Exclui comentarios desnecessarios

--- CLI Tool/obj/Debug/net7.0/CLI Tool.AssemblyInfo.cs
+++ CLI Tool/obj/Debug/net7.0/CLI Tool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLI Tool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+628de8009fc1724327969c09256c272ec7ad3c4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLI Tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLI Tool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
